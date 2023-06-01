d6e6357d822b1e4ea25708daff3573d5b94ccc5a
Quebin <gonzalesquebin@gmail.com>
2023-06-02 02:49:05 +0900

    minor fix

--- src/routes/record/[recordId]/update/+page.svelte
+++ src/routes/record/[recordId]/update/+page.svelte
@@ -224,7 +224,7 @@
 						id="dropdown"
 						name="category"
 						class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500"
-						placeholder="{selectedOption}"
+						placeholder="Select an option"
 						on:change={handleOnChange}
 					>
 						{#each options as option}
